fe3758d8950e3c0ddb0199179964ae78dec06069
Gopibabu Srungavarapu <s.gopibabu@gmail.com>
2020-05-24 20:49:45 -0500

    added display movies component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+	constructor() {
+		super();
+		// How to bind 'this' to methods in our class without arrow functions
+		this.handleIncrement = this.handleIncrement.bind(this);
+	}
 	state = {
 		count: 0,
-		tags: ['tag1', 'tag2', 'tag3'],
 	};
-	renderTags() {
-		if (this.state.tags.length === 0) return <p>There are no tags</p>;
-		return (
-			<ul>
-				{this.state.tags.map((tag) => (
-					<li key={tag}>{tag}</li>
-				))}
-			</ul>
-		);
-	}
+
+	handleIncrement = (product) => {
+		this.setState({ count: this.state.count + 1 });
+	};
+
 	render() {
 		return (
 			<React.Fragment>
 				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-				<button className='btn btn-secondary btn-sm'>Increment</button>
-				{this.renderTags()}
+				<button
+					onClick={() => this.handleIncrement()}
+					className='btn btn-secondary btn-sm'
+				>
+					Increment
+				</button>
 			</React.Fragment>
 		);
 	}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
+import 'font-awesome/css/font-awesome.css';
+import App from './App';
 
 let rootElement = document.querySelector('#root');
-ReactDOM.render(<Counter />, rootElement);
+ReactDOM.render(<App />, rootElement);
